0950b5bcf45468e418281ed2fcc87623e587dd66
RPryputen <108124300+RPryputen@users.noreply.github.com>
2023-02-23 01:34:09 +0200

    Add files via upload

--- files/sendmail/sendmail.php
+++ files/sendmail/sendmail.php
@@ -15,51 +15,55 @@
 	$mail->isSMTP();                                            //Send using SMTP
 	$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-	$mail->Username   = 'konstarsttt@gmail.com';                     //SMTP username
-	$mail->Password   = 'weis12345';                               //SMTP password
+	$mail->Username   = '';                     //SMTP username
+	$mail->Password   = '';                               //SMTP password
 	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
 	$mail->Port       = 465;                 
 	
 
-	//Від кого лист
-	$mail->setFrom('konstarsttt@gmail.com', 'Revenue Mates'); // Вказати потрібний E-mail
-	//Кому відправити
-	$mail->addAddress('dsr.egl@gmail.com'); // Вказати потрібний E-mail
-	//Тема листа
-	$mail->Subject = 'Data';
-
-	//Тіло листа
-	$body = '<h1>Зустрічайте супер листа!</h1>';
-
-	//if(trim(!empty($_POST['email']))){
-		//$body.=$_POST['email'];
-	//}	
+		//От кого письмо
+		$mail->setFrom('', 'revenue mates'); // Указать нужный E-mail
+		//Кому отправить
+		$mail->addAddress(''); // Указать нужный E-mail
+		//Тема письма
+		$mail->Subject = 'Revenue Mates - new client';
 	
-	/*
-	//Прикріпити файл
-	if (!empty($_FILES['image']['tmp_name'])) {
-		//шлях завантаження файлу
-		$filePath = __DIR__ . "/files/sendmail/attachments/" . $_FILES['image']['name']; 
-		//грузимо файл
-		if (copy($_FILES['image']['tmp_name'], $filePath)){
-			$fileAttach = $filePath;
-			$body.='<p><strong>Фото у додатку</strong>';
-			$mail->addAttachment($fileAttach);
+		//Тело письма
+		$theme .= 'new client';
+		
+		$body = 'Info:';
+		
+		if (trim(!empty($_POST['name']))) {
+			$Name .= '<strong>Name:</strong> ' . $_POST['name']; // Встив свої назви полів
 		}
-	}
-	*/
-
-	$mail->Body = $body;
-
-	//Відправляємо
-	if (!$mail->send()) {
-		$message = 'Помилка';
-	} else {
-		$message = 'Дані надіслані!';
-	}
-
-	$response = ['message' => $message];
-
-	header('Content-type: application/json');
-	echo json_encode($response);
-?>
+		if (trim(!empty($_POST['femail']))) {
+			$Email .= '<strong>Email:</strong> ' . $_POST['femail']; // Встив свої назви полів
+		}
+	
+		if (trim(!empty($_POST['fphone']))) {
+			$Tel .= '<strong>Phone:</strong> ' . $_POST['fphone']; // Встив свої назви полів
+		}
+		if (trim(!empty($_POST['ftextarea']))) {
+			$Mes .= '<strong>Message:</strong> ' . $_POST['ftextarea']; // Встив свої назви полів
+		}
+	
+		$mail->msgHTML("<html><body>
+			<h1>$body</h1>
+			<p>$Name</p>
+			<p>$Tel</p>
+			<p>$Email</p>
+			<p>$Mes</p>
+			</html></body>");
+	
+		//Отправляем
+		if (!$mail->send()) {
+			$message = 'Помилка';
+		}else {
+			$message = 'Данні відправленні!';
+		}
+	
+		$response = ['message' => $message];
+	
+		header('Content-type: application/json');
+		echo json_encode($response);
+	?>
--- js/app.js
+++ js/app.js
@@ -313,7 +313,11 @@
             }
         }));
     }
-    function functions_FLS(message) {
+    function functions_menuClose() {
+        bodyUnlock();
+        document.documentElement.classList.remove("menu-open");
+    }
+    function FLS(message) {
         setTimeout((() => {
             if (window.FLS) console.log(message);
         }), 0);
@@ -603,10 +607,48 @@
             if (!this.isOpen && this.lastFocusEl) this.lastFocusEl.focus(); else focusable[0].focus();
         }
         popupLogging(message) {
-            this.options.logging ? functions_FLS(`[Попапос]: ${message}`) : null;
+            this.options.logging ? FLS(`[Попапос]: ${message}`) : null;
         }
     }
     modules_flsModules.popup = new Popup({});
+    let gotoblock_gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 0) => {
+        const targetBlockElement = document.querySelector(targetBlock);
+        if (targetBlockElement) {
+            let headerItem = "";
+            let headerItemHeight = 0;
+            if (noHeader) {
+                headerItem = "header.header";
+                const headerElement = document.querySelector(headerItem);
+                if (!headerElement.classList.contains("_header-scroll")) {
+                    headerElement.style.cssText = `transition-duration: 0s;`;
+                    headerElement.classList.add("_header-scroll");
+                    headerItemHeight = headerElement.offsetHeight;
+                    headerElement.classList.remove("_header-scroll");
+                    setTimeout((() => {
+                        headerElement.style.cssText = ``;
+                    }), 0);
+                } else headerItemHeight = headerElement.offsetHeight;
+            }
+            let options = {
+                speedAsDuration: true,
+                speed,
+                header: headerItem,
+                offset: offsetTop,
+                easing: "easeOutQuad"
+            };
+            document.documentElement.classList.contains("menu-open") ? functions_menuClose() : null;
+            if ("undefined" !== typeof SmoothScroll) (new SmoothScroll).animateScroll(targetBlockElement, "", options); else {
+                let targetBlockElementPosition = targetBlockElement.getBoundingClientRect().top + scrollY;
+                targetBlockElementPosition = headerItemHeight ? targetBlockElementPosition - headerItemHeight : targetBlockElementPosition;
+                targetBlockElementPosition = offsetTop ? targetBlockElementPosition - offsetTop : targetBlockElementPosition;
+                window.scrollTo({
+                    top: targetBlockElementPosition,
+                    behavior: "smooth"
+                });
+            }
+            FLS(`[gotoBlock]: Юхуу...їдемо до ${targetBlock}`);
+        } else FLS(`[gotoBlock]: Йой... Такого блоку немає на сторінці: ${targetBlock}`);
+    };
     function formFieldsInit(options = {
         viewPass: false,
         autoHeight: false
@@ -731,6 +773,71 @@
             return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(formRequiredItem.value);
         }
     };
+    function formSubmit() {
+        const forms = document.forms;
+        if (forms.length) for (const form of forms) {
+            form.addEventListener("submit", (function(e) {
+                const form = e.target;
+                formSubmitAction(form, e);
+            }));
+            form.addEventListener("reset", (function(e) {
+                const form = e.target;
+                formValidate.formClean(form);
+            }));
+        }
+        async function formSubmitAction(form, e) {
+            const error = !form.hasAttribute("data-no-validate") ? formValidate.getErrors(form) : 0;
+            if (0 === error) {
+                const ajax = form.hasAttribute("data-ajax");
+                if (ajax) {
+                    e.preventDefault();
+                    const formAction = form.getAttribute("action") ? form.getAttribute("action").trim() : "#";
+                    const formMethod = form.getAttribute("method") ? form.getAttribute("method").trim() : "GET";
+                    const formData = new FormData(form);
+                    form.classList.add("_sending");
+                    const response = await fetch(formAction, {
+                        method: formMethod,
+                        body: formData
+                    });
+                    if (response.ok) {
+                        let responseResult = await response.json();
+                        form.classList.remove("_sending");
+                        formSent(form, responseResult);
+                    } else {
+                        alert("Помилка");
+                        form.classList.remove("_sending");
+                    }
+                } else if (form.hasAttribute("data-dev")) {
+                    e.preventDefault();
+                    formSent(form);
+                }
+            } else {
+                e.preventDefault();
+                if (form.querySelector("._form-error") && form.hasAttribute("data-goto-error")) {
+                    const formGoToErrorClass = form.dataset.gotoError ? form.dataset.gotoError : "._form-error";
+                    gotoblock_gotoBlock(formGoToErrorClass, true, 1e3);
+                }
+            }
+        }
+        function formSent(form, responseResult = ``) {
+            document.dispatchEvent(new CustomEvent("formSent", {
+                detail: {
+                    form
+                }
+            }));
+            setTimeout((() => {
+                if (modules_flsModules.popup) {
+                    const popup = form.dataset.popupMessage;
+                    popup ? modules_flsModules.popup.open(popup) : null;
+                }
+            }), 0);
+            formValidate.formClean(form);
+            formLogging(`Форму відправлено!`);
+        }
+        function formLogging(message) {
+            FLS(`[Форми]: ${message}`);
+        }
+    }
     let addWindowScrollEvent = false;
     setTimeout((() => {
         if (addWindowScrollEvent) {
@@ -749,4 +856,5 @@
         viewPass: false,
         autoHeight: false
     });
+    formSubmit();
 })();
